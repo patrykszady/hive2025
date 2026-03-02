287068fa4a238aa6f35b73b536b043430f0b1d5a
Patryk Szady <patryk.szady@live.com>
2026-03-01 21:59:08 -0700

    voice calls 1

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mm8o8e8o';
+const DEPLOY_VERSION = 'mm8pl6rl';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
