ec99b1c57dac6829034bc7d51cce483114ae95b5
Patryk Szady <patryk.szady@live.com>
2026-03-19 19:02:24 -0600

    timelapses and SEC

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mmveznzm';
+const DEPLOY_VERSION = 'mmy71drt';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
