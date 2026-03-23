36fdc6c5aa58760321d7e6cf98b20b8166a584c7
Patryk Szady <patryk.szady@live.com>
2026-03-23 16:43:11 -0600

    tasks 1

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mn2ocupi';
+const DEPLOY_VERSION = 'mn3ruq5c';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
