17b7943f279f9fce0181938f0ba68931560a8c62
Patryk <patryk@hive.contractors>
2026-05-06 17:20:39 +0000

    bank statements 1

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'motsz4z8';
+const DEPLOY_VERSION = 'moubop6c';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
