9f7384136521962eb1c039b3c1620fd65e7e61be
Patryk Szady <patryk.szady@live.com>
2026-05-22 19:47:50 -0600

    auto receipts

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mphorgnf';
+const DEPLOY_VERSION = 'mphov0mg';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
