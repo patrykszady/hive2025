adcf9635c627f733ae94ee1816c5623499db6544
Patryk Szady <patryk.szady@live.com>
2026-05-26 15:51:59 -0600

    yelp verify browser 1

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mpn3179p';
+const DEPLOY_VERSION = 'mpn675zl';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
