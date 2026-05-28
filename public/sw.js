a962bed7d37e75f3487e3275b45cb08bd72c09a2
Patryk Szady <patryk.szady@live.com>
2026-05-28 12:22:26 -0600

    yelp upload 76

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mpptaf1u';
+const DEPLOY_VERSION = 'mpptlj00';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
