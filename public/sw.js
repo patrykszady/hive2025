e29e3f37c3d36eeb460555de39aae33cded83eed
Patryk Szady <patryk.szady@live.com>
2026-06-21 18:06:16 -0600

    amazon

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mqofb3ry';
+const DEPLOY_VERSION = 'mqogg8ys';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
