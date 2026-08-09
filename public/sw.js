d44c12546613daa7396595e30e4fd64c8168c706
Patryk Szady <patryk.szady@live.com>
2026-08-09 14:14:36 -0500

    timelaspse 4

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'msm6gjmw';
+const DEPLOY_VERSION = 'msm6luem';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
