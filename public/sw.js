9bcbe5b4a3368892a327a08c9ba4604eccb40ae3
Patryk Szady <patryk.szady@live.com>
2026-08-21 20:40:22 -0500

    passkey 1

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'msxwsi8i';
+const DEPLOY_VERSION = 'mt3po1gq';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
