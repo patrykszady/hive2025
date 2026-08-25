c0a656c4a63ad576e9ecbcfb0cd0199e651dd55f
Patryk Szady <patryk.szady@live.com>
2026-08-25 18:52:39 -0500

    meanrds 1

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Cache version is stamped by `npm run build` so every deploy busts stale assets.
  */
 
-const DEPLOY_VERSION = 'mt6nhfny';
+const DEPLOY_VERSION = 'mt9bkf5k';
 const PAGE_CACHE  = 'hive-pages-' + DEPLOY_VERSION;
 const ASSET_CACHE = 'hive-assets-' + DEPLOY_VERSION;
 
